5b2cc38f634d39ec1097a12fe5401c9612f9519d
EmilyGlanville04 <s322158@studenti.polito.it>
2026-05-23 16:28:38 +0200

    lezione 20/05

--- UI/controller.py
+++ UI/controller.py
@@ -10,7 +10,7 @@ class Controller:
         self._choiceTeam = None
 
     def handleCreaGrafo(self, e):
-        self._model.creaGrafo()
+        self._model.creaGrafo(self._view._ddAnno.value)
         n, m = self._model.getGraphDetails()
         self._view._txt_result.controls.clear()
         self._view._txt_result.controls.append(
@@ -21,26 +21,42 @@ class Controller:
 
 
     def handleDettagli(self, e):
-        pass
+        if self._choiceTeam is None:
+            self._view._txt_result.controls.clear()
+            self._view._txt_result.controls.append(ft.Text(f"Selezionare un team dal menu.", color="red"))
+            self._view.update_page()
+            return
+
+        viciniTuple =  self._model.getVicini(self._choiceTeam)
+        self._view._txt_result.controls.clear()
+        self._view._txt_result.controls.append(ft.Text(f"Il nodo {self._choiceTeam} ha {len(viciniTuple)} vicini.", color="green"))
+        self._view._txt_result.controls.append(ft.Text(f"Di seguito una lista ordinata dei vicini.", color="green"))
+        for v in viciniTuple:
+            self._view._txt_result.controls.append(
+                ft.Text(f"{v[0]} - peso: {v[1]}", color="green"))
+
+        self._view.update_page()
 
     def handlePercorso(self, e):
         pass
 
     def _fillDDYears(self):
         years = self._model.getAllYears()
+
         # yearsDD = []
         # for y in years:
         #     yearsDD.append(ft.dropdown.Option(y))
+
         yearsDD = list(map(lambda x: ft.dropdown.Option(x), years))
         self._view._ddAnno.options = yearsDD
         self._view.update_page()
-
 
 
     def handleYearSelection(self, e):
         #questo metodo viene chiamato quando qualcuno ha selezionato un anno, deve recuperare
         #tutti i team che hanno giocato quell'anno, e stamparli nel textfield, e anche riempire
         #il dropdown sotto.
+
         if self._view._ddAnno.value is None:
             self._view._txtOutSquadre.controls.clear()
             self._view._txtOutSquadre.controls.append(ft.Text("Selezionare un anno dal menu."))
@@ -59,7 +75,6 @@ class Controller:
             )
 
         self._view.update_page()
-
 
 
     def readDDTeams(self, e):
